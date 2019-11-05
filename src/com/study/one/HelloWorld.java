8230ea214e1f056a998a5e8b9b4f95099e453b1d
e <1510213679@qq.com>
2019-11-05 17:52:44 +0800

    Java变量学习

---
+++ src/com/study/one/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.study.one;
+
+public class HelloWorld
+{
+
+	public static void main(String[] args) 
+	
+	{
+		
+		//这是一个单行注释
+		
+		/*
+		 * 这是多行注释
+		 */
+		
+		/**
+		 * 这是文档注释
+		 */
+		System.out.println("HelloWorld");
+		
+		System.out.println("我是小明");
+		
+		System.out.println("这是我的第一个Java程序");	
+		
+		System.out.println("我的数学" + 95 + "分.");
+	}
+}
---
+++ src/com/study/two/Variable.java
@@ -0,0 +1,98 @@
+package com.study.two;
+
+/**
+ * 
+ * @author Administrator
+ * @Variable 这是变量学习类
+ */
+
+public class Variable
+{
+	public static void main(String[] arge) 
+	{
+		//定义一个 int 类型的 year 变量并赋值为2019
+	
+//		int year = 2019;
+		
+		/*
+		 * 定义变量需要知道三个要素：
+		 * 类型：int
+		 * 名称：year
+		 * 值：2019
+		 */
+		
+//		int a1 = 10;
+//		
+//		int a2 = 23;
+//		
+//		int a3 = 34;
+//		
+//		System.out.println("a1 + a2 = " + (a1 + a2)); //变量的加法
+//		
+//		System.out.println("a2 × a3 = " + (a2 * a3)); //变量的乘法
+//		
+//		System.out.println("a3 ÷ a1 = " + (a3 / a1)); //变量的除法
+		
+		// int类型;可以表示正数、负数
+//		
+//		int a = 1234;
+//		
+//		int b = -293;
+		
+		//用 double 类型（双精度浮点型）表示小数
+//		
+//		double c = 12.0123;
+//		
+//		double d = -12.456;  //相当于-12.456
+		
+		//整数和小数变量根据不同的类型有不同的范围，
+		//超出了范围就会出现溢出
+		
+		//字符串 String 类型
+		
+//		String name = "小明";
+		
+		
+		/*
+		 * 小练习：
+		 * 	一个同学的名字叫小明，今年18岁，体重45.5kg
+		 * 生日2001年11月5日
+		 */
+		
+//		String name = "小明";
+//		
+//		int age = 18;
+//		
+//		double weight = 45.5;
+//		
+//		String birthday = "2001-11-5";
+//		
+//		System.out.println(name + "今年：" + age + "岁；体重：" + weight + "KG；出生日期：" +birthday);
+//		
+		
+		// boolean 类型（布尔类型）：true、false
+//		
+//		boolean a = true;
+//		
+//		boolean b = false;
+		
+		
+		
+//		String name = "小明";
+//		
+//		int age = 18;
+//		
+//		double weight = 45.5;
+//		
+//		String birthday = "2001-11-5";
+//		
+//		boolean sex = true; //true 表示男性，false 表示女
+//		
+//		if (sex) {
+//			
+//			System.out.println(name + "今年：" + age + "岁；体重：" + weight + "KG；出生日期：" +birthday);
+//		}
+		
+		
+	}
+}

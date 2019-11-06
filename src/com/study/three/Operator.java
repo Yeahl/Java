3485301c7f525abdd78578c37826acb8a51fd1d6
e <1510213679@qq.com>
2019-11-06 15:36:03 +0800

    操作符与表达式

--- src/com/study/three/Operator.java
+++ src/com/study/three/Operator.java
@@ -5,14 +5,14 @@ package com.study.three;
  * @author Administrator
  * Operator 这是操作符学习类
  * 
- * 主要学习5个操作符：
+ * 主要学习5个算术操作符：
  * 
  *  + ：加法运算
  *  - : 减法运算
  *  * ：乘法运算
  *  / : 除法运算
  *  % ：模运算
- *
+ *  
  */
 public class Operator
 
@@ -208,6 +208,140 @@ public class Operator
 //		System.out.println(d + "-" + c + "-" + b + "-" + a);
 		
 		
+		/*
+		 * 6个关系操作符：
+		 *  
+		 * 	< :小于，<=:小于等于，>:大于，
+		 *  >=:大于等于，==:等于，!=:不等于
+		 * 
+		 * 关系表达式的值为 boolean 类型（true,false)
+		 * 
+		 */
+		
+//		int a = 10, b = 4;
+//		
+//		boolean result = a > b;
+//		
+//		System.out.println(result); // true
+		
+		
+//		int a = 10;
+//		
+//		System.out.println("结果：" + ( a < 5 )); // false
+		
+		
+		/*
+		 * 3个逻辑操作符：
+		 *  
+		 * &&：逻辑与（并且）需要同时满足条件
+		 * 
+		 * ||：逻辑或（或者）只要有一个满足条件
+		 * 
+		 * ！：逻辑非（非）
+		 * 
+		 */
+		
+		
+		// 求表达式（3 > 4） && （4 > 3) 的结果
+		
+//		boolean result = ( 3 > 4 ) && ( 4 > 3 ); // false && true
+//		
+//		System.out.println(result); // false
+		
+		
+		// 求表达式 3 > 4 || 4 > 3 的值
+		
+//		boolean result = 3 > 4 || 4 > 3; // false || true
+//		
+//		System.out.println(result); // true
+		
+		// 求表达式 !(3 > 4) 的值
+		
+//		boolean result = !( 3 > 4 ); // !false
+//		
+//		System.out.println(result); // true
+		
+		
+		/*
+		 * 赋值操作符：
+		 *  
+		 * =：赋值
+		 * 
+		 */
+		
+//		int a = 10; // 定义变量a,初始值为10
+//		
+//		int b; // 如果未指定初始值，则初始值为0
+		
+//		int a = 10;
+//		
+//		a = a + 99; // 首先计算出右侧表达式（a+99）的值再赋值
+//		
+//		System.out.println(a);
+		
+		
+		/*
+		 * 自增操作符：
+		 *  
+		 * ++：自增
+		 * 
+		 */
+		
+		
+//		int a = 10;
+//		
+//		a ++; // a = a + 1 
+//		
+//		System.out.println("现在 a = "+ a);
+		
+		// 前置自增与后置自增
+		
+//		int b = 10;
+//		
+//		int a = ++ b; // 执行 ++b 后再赋值给 a; a = 11;b = 11 
+//		
+//		System.out.println(b);
+		
+//		int b = 10;
+//		
+//		int a = b ++; // 将10赋值给 a 后再执行 b ++; a = 10; b = 11
+//		
+//		System.out.println(b);
+		
+		
+		/*
+		 * 自减操作符：
+		 *  
+		 * --：自减
+		 * 
+		 */
+		
+//		int a = 0;
+//		
+//		int b = 10;
+//		
+//		a = b --; // 先将 b 赋值给 a ;再自减。所以 a = 10; b = 9
+//		
+//		System.out.println( b );
+		
+		/*
+		 * 类型转换操作符
+		 * 
+		 */
+		
+		// 整形与浮点型之间的转换
+		
+		double a = 12.33;
+		
+		int b = (int) a; // 将double类型转换成 int 类型
+		
+		System.out.println("b的值：" + b); // b = 12 
+		
+		int c = 10;
+		
+		double d = (double) c; // 将int类型转换成double类型
+		
+		System.out.println(d); // d = 10.0
 		
 	}
 

1fd8a57a050d52ce7f0977fcfb6befcc01be78aa
e <1510213679@qq.com>
2019-11-07 15:37:00 +0800

    学习数组对象的类

---
+++ src/com/study/ArrayObjects/ArrayObjects.java
@@ -0,0 +1,17 @@
+package com.study.ArrayObjects;
+
+/**
+ * 
+ * @author Administrator
+ * ArrayObjects：学习数组对象的类
+ * 
+ */
+
+public class ArrayObjects
+{
+	public static void main(String[] arge) 
+	{
+		
+	}
+
+}
